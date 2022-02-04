614665d816c57ef118fcad2a29554aa444bc8969
Nat Corpuz <nate.corpuz1@gmail.com>
2022-02-04 09:54:24 +0800

    misc

--- script.js
+++ script.js
@@ -18,21 +18,18 @@ function roundPlay(playerSelection, computerSelection) {
                 computerSelection === 'scissors' ? 
                 'You Win! Rock beats Scissors' : 'Draw! You\'re both Rock.' 
     }
-
     else if (playerSelection === 'paper') {
         return computerSelection === 'rock' ? 
                 'You Win! Paper beats Rock' :
                 computerSelection === 'scissors' ?
                 'You Lose! Scissors beats Paper' : 'Draw! You\'re both Paper.'
     }
-
     else if (playerSelection === 'scissors') {
         return computerSelection === 'rock' ?
                 'You Lose! Rock beats Scissors' :
                 computerSelection === 'paper' ?
                 'You Win! Scissors beats Paper' : 'Draw! You\'re both Scissors.'
     }
-
     else {
         gameStatus = 'end';
         return;
@@ -44,7 +41,6 @@ function game() {
     computerSelection = computerPlay();
     let result = roundPlay(playerSelection, computerSelection);
     console.log(result);
-
     return result.includes('Win') ? playerScore++ :
         result.includes('Lose') ? computerScore++ : 
         result.includes('Game') ? result : drawCounter++;
@@ -93,11 +89,9 @@ if (gameStatus === 'active') {
             i = 5;
         }
         else if (i === 4) {
-    
             console.log(`Player's score: ${playerScore}`);
             console.log(`Computer's score: ${computerScore}`);
             console.log(`Draw rounds: ${drawCounter}`);
-    
             if (playerScore > computerScore) { console.log('PLAYER WINS') }
             else if (playerScore === computerScore) { console.log('DRAW!') }
             else if (computerScore > playerScore) { console.log('COMPUTER WINS') }
